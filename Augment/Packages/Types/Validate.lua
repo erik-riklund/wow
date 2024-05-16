805d09228323a56dc7548f7fccdac5324778f381
Erik R <erik.riklund@outlook.com>
2024-05-16 14:11:09 +0200

    Update Validate.lua

--- Augment/Packages/Types/Validate.lua
+++ Augment/Packages/Types/Validate.lua
@@ -21,7 +21,15 @@ local validate = function(schema, target)
   schema = check("table", schema)
   target = check("table", target)
 
-  -- ???
+  for key, _ in pairs(target) do
+    if not schema[key] then
+      throw("The property '%s' does not exist in the specified schema", key)
+    end
+  end
+
+  for key, expected_type in pairs(schema) do
+    
+  end
 end
 
 --
