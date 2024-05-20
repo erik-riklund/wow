ba8faeed5824756acc9cbd9c4e7dfb438ce8aac7
Erik R <erik.riklund@outlook.com>
2024-05-20 22:46:42 +0200

    +

--- augment/core/events.lua
+++ augment/core/events.lua
@@ -12,6 +12,13 @@ local addon, framework = ...
 -- World of Warcraft addon ecosystem, created by Erik Riklund (2024)
 --
 
+local inspect =
+  import(
+  {
+    "debug.inspect"
+  }
+)
+
 local map, list, frame =
   import(
   {
@@ -24,3 +31,5 @@ local map, list, frame =
 local _events = list("string")
 local _listeners = map("string", "list(function)")
 local _frame = frame("augment.events", {hidden = true})
+
+-- ???
