e2cd619926203dc97a50ee4fbf65fc5c2074caf4
Erik R <erik.riklund@outlook.com>
2024-07-22 20:56:57 +0200

    +

--- Cogspinner/framework.lua
+++ Cogspinner/framework.lua
@@ -5,17 +5,49 @@
 --   \____\___/ \__, |___/ .__/|_|_| |_|_| |_|\___|_|
 --              |___/    |_|
 
+
 --#region: locally scoped global variables
 --#endregion
 
---#region [core module: mainframe]
+--#region [library: utilities] @ version 1.0.0
 --#endregion
 
---#region [core module: task processor]
+--#region [core module: core frame provider] @ version 1.0.0
 --#endregion
 
---#region [module: event handler]
+--#region [core module: task processor] @ version 1.0.0
 --#endregion
 
---#region: API @ version 1.0.0
+--#region [module: exchange manager] @ version 1.0.0
+--#endregion
+
+--#region [module: service manager] @ version 1.0.0
+--#endregion
+
+--#region [module: event handler] @ version 1.0.0
+--#endregion
+
+--#region [module: channel manager] @ version 1.0.0
+--#endregion
+
+--#region [module: data store] @ version 1.0.0
+--#endregion
+
+--#region [module: locale manager] @ version 1.0.0
+--#endregion
+
+--#region [module: tooltip controller] @ version 1.0.0
+--#endregion
+
+--#region: plugin API @ version 1.0.0
+--#endregion
+
+--#region [module: plugin manager] @ version 1.0.0
+--#endregion
+
+--#region: framework API @ version 1.0.0
+
+--- What?
+_G.cogspinner = {}
+
 --#endregion
