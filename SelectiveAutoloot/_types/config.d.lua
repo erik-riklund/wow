6fc8638fcc4db177cc859a529cc31db348437b8e
Erik R <erik.riklund@outlook.com>
2024-07-29 17:21:55 +0200

    +

---
+++ SelectiveAutoloot/_types/config.d.lua
@@ -0,0 +1,31 @@
+--- @meta
+--    _____      _           _   _
+--   / ____|    | |         | | (_)
+--  | (___   ___| | ___  ___| |_ ___   _____
+--   \___ \ / _ \ |/ _ \/ __| __| \ \ / / _ \
+--   ____) |  __/ |  __/ (__| |_| |\ V /  __/
+--  |_____/ \___|_|\___|\___|\__|_| \_/ \___|
+--     /\        | |      | |           | |
+--    /  \  _   _| |_ ___ | | ___   ___ | |_
+--   / /\ \| | | | __/ _ \| |/ _ \ / _ \| __|
+--  / ____ \ |_| | || (_) | | (_) | (_) | |_
+-- /_/    \_\__,_|\__\___/|_|\___/ \___/ \__|
+--
+--           by Ghuul (2024) v2.0
+
+--#region (type: autoloot.config)
+
+--- @class autoloot.config
+--- @field GOLD_MAX number "?"
+--- @field JUNK_ALL boolean "?"
+--- @field JUNK_MIN_VALUE number "?"
+--- @field JUNK_MAX_VALUE number "?"
+--- @field QUEST_ALL boolean "?"
+
+--#endregion
+
+--#region (alias: autoloot.configKey)
+
+--- @alias autoloot.configKey 'JUNK_ALL'|'JUNK_MIN_VALUE'|'JUNK_MAX_VALUE'
+
+--#endregion
