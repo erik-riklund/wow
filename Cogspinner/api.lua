a9baf4076b0d111ceda4b25321e2881d950734a4
Erik R <erik.riklund@outlook.com>
2024-08-24 06:43:08 +0200

    + API

--- Cogspinner/api.lua
+++ Cogspinner/api.lua
@@ -1,5 +1,3 @@
---- @meta
-
 --    ____                      _
 --   / ___|___   __ _ ___ _ __ (_)_ __  _ __   ___ _ __
 --  | |   / _ \ / _` / __| '_ \| | '_ \| '_ \ / _ \ '__|
@@ -7,22 +5,27 @@
 --   \____\___/ \__, |___/ .__/|_|_| |_|_| |_|\___|_|
 --              |___/    |_|
 
---#region [type: plugin API]
+local addon, context = ...
+--- @cast context core.context
 
---
---- ?
----
---- @class plugin.API
---
+--#region (framework context imports)
 
---#endregion
 
---#region [type: plugin options]
-
---
---- ?
----
---- @class plugin.options
---
 
 --#endregion
+
+--
+-- ?
+--
+
+--- @type API
+_G.cogspinner =
+{
+  --
+  -- ?
+  --
+
+  plugin = function(id, options)
+    ---@diagnostic disable-next-line: missing-return
+  end
+}
