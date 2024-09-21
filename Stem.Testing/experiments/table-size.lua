6762e561504797ce8c988988493cfc7d08f206e5
Erik R <erik.riklund@outlook.com>
2024-09-21 17:03:09 +0200

    +

--- Stem.Testing/experiments/table-size.lua
+++
@@ -1,4 +0,0 @@
-local numericTable = { 1, 2, 3, 4, 5 }
-local associativeTable = { a = 'alpha', b = 'beta' }
-local mixedTable = { 1, 2, a = 'alpha', 3 }
-
--- Stem/src/globals/utilities/type.lua
+++ Stem/src/globals/utilities/type.lua
@@ -1,47 +1,59 @@
 --[[
 
   Project: Stem (framework)
-  Module: ?
+  Module: Extended Type System
   Version: 1.0.0
 
   Author(s): Erik Riklund
   Created: 2024/09/20 | Updated: 2024/09/20
 
   Description:
-  ?
-
-  Dependencies: 
-
-  - ?
+  Provides a system to extend Lua's native type system by introducing new types such as 
+  `array`, `empty-table`, and `undefined`. It allows for more granular type checking in 
+  the framework.
 
   Notes:
 
-  - ?
-
-  Usage:
-
-  ?
+  - This module introduces custom types for tables, including `array` and `empty-table`, 
+    and replaces `nil` with `undefined`.
 
 ]]
 
 ---
---- ?
+--- Returns the extended type of the target. This extended system introduces `array` for 
+--- tables with sequential numeric keys, `empty-table` for empty tables, and `undefined` 
+--- for `nil` values.
 ---
---- @param value unknown "..."
---- @return extendedType "..."
+--- @param target unknown "The value to determine the extended type of."
+--- @return extendedType  "The extended type of the value."
 ---
-_G.getExtendedType = function(value)
+_G.getExtendedType = function(target)
   --
   -- ?
 
-  local valueType = type(value)
+  local valueType = type(target)
 
   -- ?
 
   if valueType == 'table' then
     -- ?
 
-    -- todo: perform tests on tables to see how the # operator behave!
+    if next(target) == nil then
+      return 'empty-table'
+    end
+
+    -- ?
+
+    local elementCount = #target
+    local actualElementCount = 0
+
+    for _ in pairs(target) do
+      actualElementCount = actualElementCount + 1
+    end
+
+    -- ?
+
+    return (elementCount == actualElementCount and 'array') or 'table'
   end
 
   -- ?
@@ -58,5 +70,28 @@ end
 --- @return boolean, extendedType "..."
 ---
 _G.compareExtendedTypes = function(value, types)
-  ---@diagnostic disable-next-line: missing-return
+  --
+  -- ?
+
+  local extendedType = getExtendedType(value)
+
+  -- ?
+
+  if type(types) ~= 'table' then
+    types = { types }
+  end
+
+  -- ?
+
+  for index, expectedType in ipairs(types) do
+    -- ?
+
+    if expectedType == extendedType then
+      return true, extendedType -- ?
+    end
+  end
+
+  -- ?
+
+  return false, extendedType
 end
