0334e8922c60f086a2b8d91a3097636032e6757f
Erik R <erik.riklund@outlook.com>
2024-10-24 20:51:08 +0200

    list + map

---
+++ Backbone/modules/config/manager.lua
@@ -0,0 +1,20 @@
+--[[~ Module: Configuration Manager ~
+  Updated: ? | Author(s): Erik Riklund (Gopher)
+]]
+
+---
+--- ?
+---
+---@class ConfigManager
+---
+local configManager = {}
+
+---
+--- ?
+---
+---@param plugin Plugin
+---@param defaults table
+---
+backbone.useConfigManager = function(plugin, defaults)
+  print 'backbone.useConfigManager: not implemented.' --
+end
--- Scavenger/plugin.lua
+++ Scavenger/plugin.lua
@@ -1,5 +1,9 @@
+---@class Scavenger
+local context = select(2, ...)
+
 --[[~ Plugin: Scavenger ~
   Updated: ? | Author(s): Erik Riklund (Gopher)
 ]]
 
 local plugin = backbone.createPlugin 'Scavenger'
+context.config = backbone.useConfigManager(plugin, {})
