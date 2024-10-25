1ae4e812be417ccf97590efa546f2c84478d75c5
Erik R <erik.riklund@outlook.com>
2024-10-25 13:14:33 +0200

    enums

--- Backbone/core/framework.lua
+++ Backbone/core/framework.lua
@@ -3,37 +3,38 @@
 ]]
 
 ---
---- A string representation of the installed version of the framework.
+--- A string representing the current version of the framework,
+--- formatted as `MAJOR.MINOR.PATCH`.
 ---
 local version = '1.0.0'
 
 ---
---- The core API for the Backbone framework.
+--- The core API for the framework, serving as the main entry point
+--- for accessing its features and functionalities.
 ---
 _G.backbone = {}
 
 ---
---- ?
+--- A module for managing components within the framework, allowing for 
+--- organization and extension of various functionalities.
 ---
 backbone.components = {}
 
 ---
---- ?
+--- A module containing enumerations used throughout the framework for 
+--- improved readability and maintainability of code.
 ---
 backbone.enums = {}
 
 ---
---- A collection of utility functions provided by the framework.
+--- An extendable collection of utility functions that assist with common
+--- tasks and operations within the framework, promoting code reusability.
+---
 ---
 backbone.utilities = {}
 
 ---
---- ?
----
-backbone.widgets = {}
-
----
 --- Returns the current version of the framework.
 ---
---- @return number, number, number "MAJOR, MINOR, and PATCH version numbers."
+--- @return number, number, number "`MAJOR`, `MINOR`, and `PATCH` version numbers."
 backbone.getCurrentVersion = function() return string.split('.', version) end
