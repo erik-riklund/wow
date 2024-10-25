9b5bc5bbeb5ef3ec7b2219da72ba91ab3f51e11e
Erik R <erik.riklund@outlook.com>
2024-10-25 14:07:38 +0200

    enums

---
+++ Backbone/enums/tradeskills.lua
@@ -0,0 +1,22 @@
+--[[~ Enum: Tradeskills ~
+  Updated: ? | Author(s): Erik Riklund (Gopher)
+]]
+
+---
+--- Defines the available tradeskill subtypes.
+---
+backbone.enums.TRADESKILL_SUBTYPES = {
+  PARTS = 1,
+  JEWELCRAFTING = 4,
+  CLOTH = 5,
+  LEATHER = 6,
+  METAL_AND_STONE = 7,
+  COOKING = 8,
+  HERB = 9,
+  ELEMENTAL = 10,
+  OTHER = 11,
+  ENCHANTING = 12,
+  INSCRIPTION = 16,
+  OPTIONAL_REAGENTS = 18,
+  FINISHING_REAGENTS = 19,
+}
