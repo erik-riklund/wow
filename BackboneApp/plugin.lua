cf0a398b7798d3ee58ba7744348f17b2840038ef
Erik R <erik.riklund@outlook.com>
2024-11-11 22:28:07 +0100

    +

--- BackboneApp/plugin.lua
+++
@@ -1,8 +0,0 @@
----@class BackboneApp
-local context = select(2, ...)
-
---[[~ Plugin: Backbone Application ~
-  Updated: ? | Author(s): Erik Riklund (Gopher)
-]]
-
-local plugin = backbone.createPlugin 'BackboneApp'
---
+++ BackboneConsole/plugin.lua
@@ -0,0 +1,8 @@
+---@class BackboneConsole
+local context = select(2, ...)
+
+--[[~ Plugin: Backbone Console ~
+  Updated: 2024/11/11 | Author(s): Erik Riklund (Gopher)
+]]
+
+local plugin = backbone.createPlugin 'BackboneConsole'
