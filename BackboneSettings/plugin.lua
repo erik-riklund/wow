8b091dee371cfca8e774b71e5340bf173f8f1a3d
Erik R <erik.riklund@outlook.com>
2024-11-12 08:54:53 +0100

    +

--- BackboneSettings/plugin.lua
+++ BackboneSettings/plugin.lua
@@ -1,8 +1,8 @@
 ---@class BackboneConsole
 local context = select(2, ...)
 
---[[~ Plugin: Console (UI) ~
+--[[~ Plugin: Settings (UI) ~
   Updated: 2024/11/11 | Author(s): Erik Riklund (Gopher)
 ]]
 
-local plugin = backbone.createPlugin 'BackboneConsole'
+local plugin = backbone.createPlugin 'BackboneSettings'
