d8513e7c064b3626599eeabfaf8ecfc52c9df644
Erik R <erik.riklund@outlook.com>
2024-11-25 19:34:53 +0100

    +

---
+++ Scavenger/initialize.lua
@@ -0,0 +1,15 @@
+---@class Scavenger
+local context = select(2, ...)
+
+--[[~ Updated: 2024/11/21 | Author(s): Erik Riklund (Gopher) ]]
+
+---
+--- Create the plugin.
+---
+context.plugin = backbone.createPlugin 'Scavenger'
+
+---
+--- Create the channel used to broadcast a list of items that remains
+--- after the looting process is completed.
+---
+context.plugin:createChannel 'LOOT_PROCESSED'
