681446c9acd8c2d98ce1b0d7d56f0df29a7cad8c
Erik R <erik.riklund@outlook.com>
2026-05-17 11:38:08 +0200

    Add settings registry and API stubs
    Introduce a registry for module settings and add scavenger.extend helpers: register_settings (stores settings per module and warns on duplicates), plus placeholder stubs for override_setting and get_setting. Also tidy up header comments and comment out settings.lua in Scavenger.toc. Override/get implementations are left as TODOs.

--- scavenger/core/settings.lua
+++ scavenger/core/settings.lua
@@ -4,13 +4,63 @@
 --  ___) | (_| (_| |\ V /  __/ | | | (_| |  __/ |
 -- |____/ \___\__,_| \_/ \___|_| |_|\__, |\___|_|
 --                                  |___/
---   github.com/erik-riklund/wow/scavenger/core (2026)
+-- github.com/erik-riklund/wow/scavenger/core (2026)
 
 ---@class context
 local x = select(2, ...)
 
 --
 -- # ?
--- 
+--
 -- ...
 --
+
+local registry = {}
+
+--
+-- # ?
+--
+-- ...
+--
+
+scavenger.extend(
+  "register_settings", function(module, settings)
+    if registry[module] == nil then
+      registry[module] = {}
+    end
+
+    for key, value in pairs(settings) do
+      if registry[module][key] ~= nil then
+        scavenger.warn(
+          "Setting '%s/%s' already exists, use 'scavenger.override_setting'", module, key
+        )
+      else
+        registry[module][key] = value
+      end
+    end
+  end
+)
+
+--
+-- # ?
+--
+-- ...
+--
+
+scavenger.extend(
+  "override_setting", function(path, value)
+    -- ?
+  end
+)
+
+--
+-- # ?
+--
+-- ...
+--
+
+scavenger.extend(
+  "get_setting", function(path)
+    -- todo > ?
+  end
+)
